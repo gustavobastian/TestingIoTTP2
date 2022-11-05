071b8418c626dde5903ecea5f9fdb3aca163e023
Gustavo Bastian <bastian.gustavo@gmail.com>
2022-11-05 15:53:02 -0300

    gana jugador 1, agrego que la respuesta debe ser un objeto

--- test/specs.js
+++ test/specs.js
@@ -20,21 +20,28 @@ chai.use(chaihttp)
 
 
 
-describe("juego de tateti", async ()=>{
-    
-    
-    
+describe("juego de tateti", async ()=>{ 
+    let movimientos = [
+        { jugador: 'Juan', columna: 0, fila: 0 },
+        { jugador: 'Pedro', columna: 1, fila: 0 },
+        { jugador: 'Juan', columna: 0, fila: 1 },
+        { jugador: 'Pedro', columna: 1, fila: 1 },
+        { jugador: 'Juan', columna: 0, fila: 2 },
+        { jugador: 'Pedro', columna: 0, fila: 0 },
+    ];
+    let juego = ['Juan','Pedro'];
+            
     describe("empieza juego nuevo", async()=>{
 
         it ("Todos los casilleros estan vacios", (done)=>{
-            let juego = ['Juan','Pedro']
-
+            
             chai.request(server)
             .put("/empezar")
             .send(juego)
             .end((err,res)=>{
                 res.should.have.status(200);            
                 res.should.to.be.json;       
+                res.should.to.be.a('object');  
                 res.body.should.have.property('turno').eql('Juan');
                 res.body.should.have.property('estado').eql([
                     [' ',' ',' '],
@@ -49,13 +56,14 @@ describe("juego de tateti", async ()=>{
             /*let juego = {
                 jugadores: ['Juan','Pedro']
             }*/
-            let juego = ['Juan','Pedro']
+            
             chai.request(server)
             .put("/empezar")
             .send(juego)
             .end((err,res)=>{            
                 res.should.have.status(200);            
-                res.should.to.be.json;       
+                res.should.to.be.json;    
+                res.should.to.be.a('object');     
                 res.body.should.have.property('turno').eql('Juan');
                 res.body.should.have.property('estado').eql([
                     [' ',' ',' '],
@@ -70,15 +78,7 @@ describe("juego de tateti", async ()=>{
     })
 
     describe(" movimientos", ()=>{
-        let movimientos = [
-            { jugador: 'Juan', columna: 0, fila: 0 },
-            { jugador: 'Pedro', columna: 1, fila: 0 },
-            { jugador: 'Juan', columna: 0, fila: 1 },
-            { jugador: 'Pedro', columna: 1, fila: 1 },
-            { jugador: 'Juan', columna: 0, fila: 2 },
-            { jugador: 'Pedro', columna: 0, fila: 0 },
-        ]
-        let juego = ['Juan','Pedro']
+        
         it ("El casillero queda ocupado y le toca al otro jugador", (done)=>{
             chai.request(server).put("/empezar").send(juego).end();            
             chai.request(server)
@@ -86,7 +86,8 @@ describe("juego de tateti", async ()=>{
             .send(movimientos[0])
             .end((err,res)=>{
                 res.should.have.status(200);            
-                res.should.to.be.json;       
+                res.should.to.be.json;     
+                res.should.to.be.a('object');    
                 res.body.should.have.property('turno').eql('Pedro');
                 res.body.should.have.property('estado').eql([
                     ['x',' ',' '],
@@ -104,7 +105,8 @@ describe("juego de tateti", async ()=>{
             .send(movimientos[1])
             .end((err,res)=>{
                 res.should.have.status(200);            
-                res.should.to.be.json;                       
+                res.should.to.be.json;     
+                res.should.to.be.a('object');                    
                 res.body.should.have.property('turno').eql('Juan');
                 res.body.should.have.property('estado').eql([
                     ['x','0',' '],
@@ -114,6 +116,7 @@ describe("juego de tateti", async ()=>{
                 done();
             })
         })
+
         it ("no debe aceptar movimientos de jugadores que no le corresponden ", (done)=>{
             chai.request(server).put("/empezar").send(juego).end();
             chai.request(server).put("/movimiento").send(movimientos[0]).end();
@@ -122,7 +125,8 @@ describe("juego de tateti", async ()=>{
             .send(movimientos[2])
             .end((err,res)=>{
                 res.should.have.status(200);            
-                res.should.to.be.json;                       
+                res.should.to.be.json;     
+                res.should.to.be.a('object');                    
                 res.body.should.have.property('turno').eql('Pedro');
                 res.body.should.have.property('estado').eql([
                     ['x',' ',' '],
@@ -141,7 +145,8 @@ describe("juego de tateti", async ()=>{
             .send(movimientos[5])
             .end((err,res)=>{
                 res.should.have.status(200);            
-                res.should.to.be.json;                       
+                res.should.to.be.json;    
+                res.should.to.be.a('object');                     
                 res.body.should.have.property('turno').eql('Pedro');
                 res.body.should.have.property('estado').eql([
                     ['x',' ',' '],
@@ -150,6 +155,33 @@ describe("juego de tateti", async ()=>{
                 ]);
                 done();
             })
+        })
+
+    })
+    describe(" el primer jugador gana la partida", ()=>{
+
+        it("el juego termina cuando hay 3 valores iguales",(done)=>{
+            chai.request(server).put("/empezar").send(juego).end();
+            chai.request(server).put("/movimiento").send(movimientos[0]).end();
+            chai.request(server).put("/movimiento").send(movimientos[1]).end();
+            chai.request(server).put("/movimiento").send(movimientos[2]).end();
+            chai.request(server).put("/movimiento").send(movimientos[3]).end();
+            chai.request(server).put("/movimiento").send(movimientos[4])
+            .end((err,res)=>{
+                res.should.have.status(200);            
+                res.should.to.be.json;                       
+                res.should.to.be.a('object');              
+                res.body.should.have.property('turno').eql('Pedro');
+                res.body.should.have.property('estado').eql([
+                    ['x','0',' '],
+                    ['x','0',' '],
+                    ['x',' ',' '],
+                ]);
+                done();
+            })
+
+
+
         })
 
     })
